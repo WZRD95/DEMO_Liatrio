1c41a7fa89c4892fe36001402ec25fe9780f26ca
root <root@WZRDPC>
2024-10-07 14:15:38 -0700

    Added index.js it does work on Local machine  10/7/2024 (#1)

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # DEMO_Liatrio
 Here is the code for the various programs from the Exercise 
+(#0)
+>This is a test for git push 10/7/2024 1.57
 
-This is a test for git push 10/7/2024 1.57
+(#1)
+>Added index.js as for now it does work and run with no issues it can LOCALHOST
+
+
+
---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const jsonminify = require('jsonminify')
+const get_date = new Date().toLocaleString()
+const PORT = process.env.PORT || 80;
+
+app.listen(PORT, () => {
+    console.log(`\n  Run time start => ${get_date}\n`);
+    console.log(`  Server running on port ${PORT}\n`);
+});
+
+app.get('/', function (request, response){
+    var time_out = new Date().toLocaleString();
+    const my_Json = { "message": "My name is Joel", "timestamp": time_out }
+    const minified_Json = jsonminify(JSON.stringify(my_Json));
+
+    if(request)  
+    { 
+        console.log("New Request..."); 
+        console.log("    Log-> " + request.header('user-agent') + "\n"); 
+    }
+
+    response.send(minified_Json);
+})
+
+// minify bases off this 
+//      https://www.npmjs.com/package/jsonminify
+
+// the index name convention base off of the idea that it will work with pakage JSON
+//      https://stackoverflow.com/questions/19850028/what-naming-conventions-exist-for-the-primary-node-js-file
