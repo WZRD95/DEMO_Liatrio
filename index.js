28f3f253534bc5ab2109ff0f286900050fc0c3b6
root <root@WZRDPC>
2024-10-08 21:54:28 -0700

    Time needed getTime fix 6  10/8/2024

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', function (request, response){
     const minified_Json = jsonminify(JSON.stringify(my_Json));
 
     response.setHeader('Content-Type', 'application/json');
-    response.end(JSON.stringify(minified_Json));
+    response.send(minified_Json);
 
     //response.send(minified_Json);
 })
